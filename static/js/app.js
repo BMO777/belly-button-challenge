5de31d99a9fe3e46fc07740c8d2204162f8385ae
BMO777 <bomirera@gmail.com>
2023-04-03 16:28:15 -0500

    resolve uncaught reference error and use different function for onchange

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 // Get the samples.json endpoint
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 // Fetch the JSON data and run functions to create plot within it
-d3.json(url).then(data2 = (data) => {
+d3.json(url).then((data) => {
     console.log('data: ', data);
     //get all values within endpoint
     let meta = Object.values(data.metadata);
@@ -17,16 +17,18 @@ d3.json(url).then(data2 = (data) => {
 
     //populate charts and demographics with random value initially
     random = ids[ids.length * Math.random() | 0];
-    //populate drop down with initial value
+    //populate drop down menu with initial value
     d3.select("#selDataset").property("value", `${random}`) ;
 
     // console.log('randomvs: ', meta.filter(ds => ds.id == (random))[0]);
     // console.log('random: ', (random));
-    //selected data -sd
+    //selected data id =sd
     graph = (sd) => {
+        //bar chart trace and 
         let dbar = [{
             
             //id in .names is common in all arrays so it is filtered and accompanying data is extracted
+            //then slice value to ten values convienienty ordered to get the top ten, then reverse for plotly
             x: samples.filter(ds => ds.id == (sd))[0].sample_values.slice(0, 10).reverse(),
             y: samples.filter(ds => ds.id == (sd))[0].otu_ids.slice(0, 10).reverse().map(a => `OTU ${a}`),
             text: samples.filter(ds => ds.id == (sd))[0].otu_labels.slice(0, 10).reverse(),
@@ -46,7 +48,7 @@ d3.json(url).then(data2 = (data) => {
         }];
             //clear demographics box before change
             d3.select("#sample-metadata").html('');
-            //filter once again based on sd id in metadata and use colon and space, instead of comma to separate values in Object.entries
+            //filter once again based on sd id in metadata and use colon and space, instead of default comma to separate values in Object.entries
             dict = Object.entries(meta.filter(ds => ds.id == (sd))[0]).map(e => e.join(': '));
         //.entries is like .items in python
             for (let item in dict)
@@ -54,50 +56,19 @@ d3.json(url).then(data2 = (data) => {
             Plotly.newPlot("bar", dbar);
             Plotly.newPlot("bubble", dbubble);
         };
+        //initially call random value within graph function holding plotting data
     init = () => {graph(random)}; 
- 
-    function optionChanged()  {
-                //get id from dropdown selection
-        let id = d3.selectAll("#selDataset").property("value")
-        console.log('selectid: ', id) 
-    
-        let dbar = [{
-            
-            //id in .names is common in all arrays so it is filtered and accompanying data is extracted
-            x: samples.filter(ds => ds.id == (id))[0].sample_values.slice(0, 10).reverse(),
-            y: samples.filter(ds => ds.id == (id))[0].otu_ids.slice(0, 10).reverse().map(a => `OTU ${a}`),
-            text: samples.filter(ds => ds.id == (id))[0].otu_labels.slice(0, 10).reverse(),
-            type: "bar",
-            orientation: 'h'
-        }];
-        let dbubble = [{
-
-            x: samples.filter(ds => ds.id == (id))[0].otu_ids,
-            y: samples.filter(ds => ds.id == (id))[0].sample_values,
-            text: samples.filter(ds => ds.id == (id))[0].otu_labels,
-            mode: "markers",
-            marker: {
-                size: samples.filter(ds => ds.id == (id))[0].sample_values,
-                color: samples.filter(ds => ds.id == (id))[0].otu_ids
-            },
-        }];
-            d3.select("#sample-metadata").html('')
-            //filter once again based on id in metadata and use colon and space, instead of comma to separate values in Object.entries
-            dict = Object.entries(meta.filter(ds => ds.id == (id))[0]).map(e => e.join(': '))
-        //.entries is like .items in python
-            for (let item in dict)
-                {d3.select(".panel-body").append("h6").text(`${dict[item]}`)};
-
-                Plotly.newPlot("bar", dbar);
-                Plotly.newPlot("bubble", dbubble);}
-
-        d3.select("#selDataset").on("change", optionChanged);
-
-        init();
-
+    init();
+//get selected value by id https://www.techiedelight.com/get-selected-value-dropdown-javascript-onchange/
+ //get id (this.value)from dropdown selection and call function that calls graph function referencing selected id
+    document.getElementById('selDataset').onchange = function() {
+            graph(this.value);
+        };
     });
-    
-//   data2()
-        console.log('data2: ', data2);
 
+    // optionChanged = () => {graph(this.value)}
 
+    // function optionChanged()  {
+    //            
+    //     // let id = d3.select("#selDataset").property("value")
+    //     graph(this.value)};
